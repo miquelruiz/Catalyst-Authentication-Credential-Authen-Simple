dcdeaeb37303eda0bccebb337cf13f211570fd84
Jose Luis Martinez Torres <CENSORED>
2009-11-04 04:51:37 -0800

    import Catalyst-Authentication-Credential-Authen-Simple 0.06 from CPAN
    git-cpan-module:   Catalyst-Authentication-Credential-Authen-Simple
    git-cpan-version:  0.06
    git-cpan-authorid: JLMARTIN
    git-cpan-file:     authors/id/J/JL/JLMARTIN/Catalyst-Authentication-Credential-Authen-Simple-0.06.tar.gz

--- t/lib/AuthTestApp4.pm
+++ t/lib/AuthTestApp4.pm
@@ -3,25 +3,6 @@ package AuthTestApp4;
 use TestLogger;
 use Catalyst qw/Authentication/;
 
-use Test::More;
-use Test::Exception;
-
-sub authed_ok : Local {
-	my ( $self, $c ) = @_;
-
-        my $authd = $c->authenticate( { "username" => $c->request->param('username'),
-	                               "password" => $c->request->param('password') });
-
-	if ($authd){
-	    $c->response->body( "authed " . $c->user->get('name') );
-	} else {
-            $c->response->body( "not authed" );
-	}
-
-        $c->logout;
-}
-
-
 __PACKAGE__->config->{'Plugin::Authentication'} = {
   'realms' => {
     'default' => {
